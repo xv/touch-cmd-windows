6acc1213939c32cc09416729d1ad8903361f417b
Jad Altahan <xviyy@aol.com>
2023-05-05 11:25:42 -0700

    Improve PATH installation script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -25,45 +25,48 @@ if (-Not (Test-Path $cmdPath)) {
     Exit
 }
 
+# Check if installed in the temp/session PATH
 $installed = $null -ne (
     Get-Command $cmdName `
         -Type Application `
         -ErrorAction Ignore
 ).Path
 
-if ($installed) {
-    Write-Host "Directory '$PSScriptRoot' already exists in PATH." -f Green
-    Exit
+if (-Not $installed) {
+    # Add the script/command dir to the temp. session PATH so that the command
+    # can be used instantly without having to restart the terminal
+    if ($env:PATH -notlike "*$PSScriptRoot*") {
+        if (-Not $env:PATH.EndsWith(";")) {
+            $env:PATH += ";"
+        }
+
+        $env:PATH += $PSScriptRoot
+    }
 }
 
 $regEnvPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 $regPathVal = (Get-ItemProperty $regEnvPath).Path
 
-if (-Not $regPathVal.EndsWith(";")) {
-    $regPathVal += ";"
-}
-
-$cmdEnvPath = -join ($regPathVal, $PSScriptRoot)
-
-if ($cmdEnvPath.Length -le $regPathVal.Length) {
-    Write-Host "Could not join command path with environment PATH." -f Red
+if (($regPathVal -like "*$PSScriptRoot*") -and $installed) {
+    Write-Host "Directory '$PSScriptRoot' already exists in PATH." -f Green
     Exit
-}
-
-# Add the script/command dir to the permanent PATH in registry
-Set-ItemProperty `
-    -Path $regEnvPath `
-    -Name Path `
-    -Value $cmdEnvPath
-
-# Add the script/command dir to the temp. session PATH so that the command
-# can be used instantly without having to restart the terminal
-if ($env:PATH -notlike "*$PSScriptRoot*") {
-    if (-Not $env:PATH.EndsWith(";")) {
-        $env:PATH += ";"
+} else {
+    if (-Not $regPathVal.EndsWith(";")) {
+        $regPathVal += ";"
     }
 
-    $env:PATH += $PSScriptRoot
+    $cmdEnvPath = -join ($regPathVal, $PSScriptRoot)
+
+    if ($cmdEnvPath.Length -ne ($regPathVal.Length + $PSScriptRoot.Length)) {
+        Write-Host "Could not join command path with environment PATH." -f Red
+        Exit
+    }
+
+    # Add the script/command dir to the permanent PATH in registry
+    Set-ItemProperty `
+        -Path $regEnvPath `
+        -Name Path `
+        -Value $cmdEnvPath
 }
 
 Write-Host "Directory '$PSScriptRoot' has been added to PATH." -f Green
